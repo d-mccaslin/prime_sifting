8e9e830753856acb49e34273bfa44140f94116ef
David McCaslin <davidmccaslin94@gmail.com>
2020-09-24 16:46:47 -0700

    update prime_sift method
    Co-Authored by: Adam Mansell <adammansellhmb@gmail.com>
    Co-authored by: Forrest Reiland <freiland@email.arizona.edu>

--- lib/prime.rb
+++ lib/prime.rb
@@ -2,8 +2,15 @@ require('pry')
 
 class Integer
   def prime_sift
-    result = (2..self)
-  
-    return result
+    range = (2..self)
+    prime = 2
+
+    range.each do |range_element|
+      if range_element % prime == 0
+        puts range_element
+      end
+    end
+    return range
+
   end
 end
