040bde3ce31b7a95dd7e57d348bf32d647b7a4fd
David McCaslin <davidmccaslin94@gmail.com>
2020-09-24 14:35:33 -0700

    add first real spec

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -2,7 +2,14 @@ require('rspec')
 require('prime')
 
 describe('Ineger#prime_sift') do
-  it('returns a number') do
-    expect(5.prime_sift).to(eq(10))
+
+  # it("returns a number") do
+  #   expect(5.prime_sift).to(eq(10))
+  # end
+
+  it("Create a list of numbers from 2 through n: 2, 3, 4, ..., number") do
+    expect(5.prime_sift).to(eq([1,2,3,4,5]))
   end
+
+
 end
